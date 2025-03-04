48c91964bdf081e5d0e563f58a64cd5e5090918c
hwaeop <fkzktym@naver.com>
2025-03-04 17:07:27 +0900

    fahr2celsius.c

---
+++ c_example/charNumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch1='A';
+    char ch2=65;
+    char ch3;
+
+
+    printf("ch1: %c Num: %d\n",ch1,ch1);
+    printf("ch2: %c Num: %d\n",ch2,ch2);
+    printf("Input A character : ");
+    scanf("%c",&ch3);
+    printf("ch3: %c Num: %d",ch3,ch3);
+
+    return 0;
+}
