b4cbea83ca37089b7482080bf48d97cb5da3f0cd
hwaeop <fkzktym@naver.com>
2025-03-04 15:21:16 +0900

    celsisus2fahr.c

---
+++ c_example/celsisus2fahr.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int celsius=15;
+    double fahr;
+    fahr= celsius*9/5+32;
+    printf("celsius temp: %d \n",celsius);
+    printf("fahr temp: %f \n",fahr);
+
+    return 0;
+}
