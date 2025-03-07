0e60df0cb03c0a0b628822a5a5aa2dbc2f1a14ef
hwaeop <fkzktym@naver.com>
2025-03-07 09:42:50 +0900

    2025-03-06

---
+++ c_example/part2/one2TenDoWhile.c
@@ -0,0 +1,15 @@
+//1~50까지 Dowhile로 출력하기
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(void)
+{
+    int i=0;
+    do
+    {
+        printf("%d 번 출력입니다.\n",i+1);
+        i++;
+    }
+    while(i<10);
+    return 0;
+}
