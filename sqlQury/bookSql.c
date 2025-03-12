7ad4002414837b212cbd662b0ca5b7c4ecb43667
hwaeop <fkzktym@naver.com>
2025-03-12 14:01:16 +0900

    SQL setting & insert

---
+++ sqlQury/bookSql.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <mysql.h> //sudo apt install libmysql++*
+//dpkg -L libmysqlclient-dev | grep mysql.h
+// cc -o bookSql bookSql.c -I/usr/include/mysql -L/usr/lib/mysql -lmysqlclient
+int main(void)
+{
+
+    return 0;
+}
