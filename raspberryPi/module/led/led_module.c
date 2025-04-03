4284522e2cd9dacdd748281e367064dfd73ff10f
Hwa-seop <fkzktym@naver.com>
2025-04-03 17:13:17 +0900

    0403_new

--- raspberryPi/module/led/led_module.c
+++ raspberryPi/module/led/led_module.c
@@ -1,24 +1,25 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
 #include <linux/fs.h>
 #include <linux/gpio.h>
-#include <linux/uaccess.h>
-#include <linux/asm/io.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
 #define HIGH 1
 #define LOW 0
-int led[4]={23,24,25,1};
+
+int led[4] = {23 + 512, 24 + 512, 25 + 512, 1 + 512};
 
 static int led_module_init(void)
 {
-    int ret,i;
-    printk(KERN_INFO "led module init!\n");
+    int ret, i;
+    printk(KERN_INFO "led module init! \n");
     for (i = 0; i < 4; i++)
     {
         ret = gpio_request(led[i], "LED");
         if (ret < 0)
-            printk(KERN_INFO "led module gpio request failed!\n");
+            printk(KERN_INFO "led mosule gpio request failed!\n");
     }
     for (i = 0; i < 4; i++)
     {
@@ -31,12 +32,17 @@ static void led_module_exit(void)
 {
     int i;
     printk(KERN_INFO "led module exit!\n");
-    for(i=0;i<4;i++)
+    for (i = 0; i < 4; i++)
+    {
+        gpio_direction_output(led[i], LOW);
+    }
+    for (i = 0; i < 4; i++)
     {
         gpio_free(led[i]);
     }
-    
+    printk(KERN_INFO "All GPIOs freed successfully.\n");
 }
+
 module_init(led_module_init);
 module_exit(led_module_exit);
 MODULE_LICENSE("GPL");
