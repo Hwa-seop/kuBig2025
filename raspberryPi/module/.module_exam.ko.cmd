ddd8bdaeb3bb8cbc02d21aab0bde12dc9708fb59
Hwa-seop <fkzktym@naver.com>
2025-04-03 17:13:17 +0900

    0403_new

--- raspberryPi/module/.module_exam.ko.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hs/kuBig2025/raspberryPi/module/module_exam.ko := ld -r -EL  -maarch64elf -z noexecstack --no-warn-rwx-segments --build-id=sha1  -T arch/arm64/module.lds -o /home/hs/kuBig2025/raspberryPi/module/module_exam.ko /home/hs/kuBig2025/raspberryPi/module/module_exam.o /home/hs/kuBig2025/raspberryPi/module/module_exam.mod.o;  true
--- raspberryPi/module/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hs/kuBig2025/raspberryPi/module/modules.order := {   echo /home/hs/kuBig2025/raspberryPi/module/module_exam.o; :; } > /home/hs/kuBig2025/raspberryPi/module/modules.order
