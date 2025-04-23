de6b2bc77974c4fc5fe0e4059e491fc71f811d5b
hwaseop <fkzktym@naver.com>
2025-04-23 20:40:13 +0900

    c++_day1

---
+++ cpp/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+/*
+int main()
+{
+    std::cout<<"hello world"<<std::endl;
+    operator << (std::cout,"hello") <<(std::endl);
+    // std::cout.operator<<("hello") <<std::endl;
+    // cout<<"hello world"<<endl;
+}
+*/
+
+using namespace std;
+int main()
+{
+    cout << "hello" << endl;
+    cout << "a is b" << endl;
+    return 0;
+}
