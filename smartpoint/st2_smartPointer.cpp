45fef0a9203b0483728b3be9e500652569de10bb
hwaseop <fkzktym@naver.com>
2025-04-24 22:32:56 +0900

    smart_pointer

--- smartpoint/st2_smartPointer.cpp
+++ smartpoint/st2_smartPointer.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+// --------unique_ptr----------
 // int main()
 // {
 //     // unique_ptr<int> ptr = make_unique<int>(10); // 동적 생성
@@ -15,20 +16,27 @@ using namespace std;
 //     cout << *p2 << endl; // 출력: 10
 // }
 
-// shared_ptr
+// -------shared_ptr-------
+// int main()
+// {
+//     // shared_ptr<int> a = make_shared<int>(20);
+//     auto a = make_shared<int>(30);
+//     shared_ptr<int> b = a;
+
+//     cout << a.use_count() << endl; // 2 (a, b 공유 중), 갯수
+//     cout << *a << endl;            // 30
+//     cout << *b << endl;            // 30
+// }
+
+// ------------weak_ptr---------------
+
 int main()
 {
-    shared_ptr<int> a = make_shared<int>(20);
-    shared_ptr<int> b = a;
+    shared_ptr<int> a = make_shared<int>(100);
+    weak_ptr<int> w = a;
 
-    cout << a.use_count() << endl; // 2 (a, b 공유 중)
-    cout << b.use_count() << endl; // 2 (a, b 공유 중)
+    if (auto sp = w.lock())
+    {
+        cout << *sp << endl; // 100
+    }
 }
-
-// weak_ptr
-//  shared_ptr<int> a = make_shared<int>(100);
-//  weak_ptr<int> w = a;
-
-// if (auto sp = w.lock()) {
-//     cout << *sp << endl; // 100
-// }
