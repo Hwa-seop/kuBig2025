577495bec2b37525c0942e68dfd1c9cc2a586c54
hwaseop <fkzktym@naver.com>
2025-04-24 17:29:32 +0900

    update

--- smartpoint/ex1_uniquepoint.cpp
+++ smartpoint/ex1_uniquepoint.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <memory>
 using namespace std;
-class Myclass{
+class Myclass
+{
 public:
     int test_a;
-    Myclass() 
-    { 
-        cout<<"Myclass 생성!!" <<endl;
-        test_a=100;
+    Myclass() // 생성자
+    {
+        cout << "Myclass 생성!!" << endl;
+        test_a = 100;
     }
-    ~Myclass() {cout<<"Myclass 소멸!!"<<endl;}
+    ~Myclass() { cout << "Myclass 소멸!!" << endl; } // 소멸자
 };
 
 int main()
 {
-    auto ptr=make_unique<Myclass>(); // unique_ptr<Myclass> ptr=make_unique<Myclass>();
-    //heap에 만들어진 메모리.
-    ptr->test_a=100;
-    cout<<ptr->test_a<<endl;
+    auto ptr = make_unique<Myclass>(); // unique_ptr<Myclass> ptr=make_unique<Myclass>();
+    // heap에 만들어진 메모리.
+    ptr->test_a = 100;
+    cout << ptr->test_a << endl;
+
     return 0;
 }
