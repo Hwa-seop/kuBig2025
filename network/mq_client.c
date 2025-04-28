17537da42ad1ffcce34cc87d46dbcd6221944371
hwaseop <fkzktym@naver.com>
2025-04-28 16:48:29 +0900

    socket

---
+++ network/mq_client.c
@@ -0,0 +1,24 @@
+//서버 실행 하고
+//build/network 에서 ./mq_server 실행, ./mq_client
+
+#include <fcntl.h>
+#include <mqueue.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define QUEUE_NAME "/myqueue"
+#define MAX_MSG_SIZE 128
+
+int main()
+{
+    mqd_t mq=mq_open(QUEUE_NAME,O_WRONLY);
+    
+    char buffer[]="Hello from Client!!";
+
+    mq_send(mq,buffer,strlen(buffer)+1,0);
+    printf("서버로 보낸 메세지: %s\n",buffer);
+
+    mq_close(mq);
+    return 0;
+}
